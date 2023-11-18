33bef99ced720c5668a08395ce134f5ba5d93ca3
phil3838 <cote3838@gmail.com>
2023-11-18 13:25:37 -0500

    famille brown and cancel bad answer sound

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -117,7 +117,8 @@ export class AppComponent implements OnInit {
     this.meanOfTimes = mean.toFixed(2);
   }
 
-  cancelLastAnswer() {
+  cancelLastAnswer() {      
+    this.correctAnswerSound();
     this.isCorrect = true;
     this.wrongAnswerCtn--;
     this.correctAnswerCtn++;
